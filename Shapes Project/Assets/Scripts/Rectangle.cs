e8a15bbc8425d6f1bc8ea1764c66c41357903e21
MadMoth125 <madmoth125@gmail.com>
2023-09-11 16:38:20 -0500

    added Rectangle script

---
+++ Shapes Project/Assets/Scripts/Rectangle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rectangle : Shape
+{
+    public float Length { get; private set; }
+    public float Width { get; private set; }
+    
+    public Rectangle(float length, float width)
+    {
+        Length = length;
+        Width = width;
+    }
+    
+    public override float CalculateArea()
+    {
+        Area = Length * Width;
+        return Area;
+    }
+    
+    public override float CalculatePerimeter()
+    {
+        Perimeter = 2 * (Length + Width);
+        return Perimeter;
+    }
+}
