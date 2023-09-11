ed2c728ce75bc523ab6352bd692ee4619a0c952a
MadMoth125 <madmoth125@gmail.com>
2023-09-11 14:26:30 -0500

    added Circle class

---
+++ Shapes Project/Assets/Scripts/Circle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : Shape
+{
+    public float Radius { get; private set; }
+    
+    public Circle(float radius)
+    {
+        Radius = radius;
+    }
+    
+    public override float CalculateArea()
+    {
+        Area = Mathf.PI * Mathf.Pow(Radius, 2);
+        return Area;
+    }
+    
+    public override float CalculatePerimeter()
+    {
+        Perimeter = 2 * Mathf.PI * Radius;
+        return Perimeter;
+    }
+}
