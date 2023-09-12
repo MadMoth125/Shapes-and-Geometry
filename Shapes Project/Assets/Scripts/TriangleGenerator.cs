a50d7a2f3314355e65a421df872f4893811e9055
MadMoth125 <madmoth125@gmail.com>
2023-09-12 09:33:11 -0500

    removed old triangle script

--- Shapes Project/Assets/Scripts/TriangleGenerator.cs
+++ Shapes Project/Assets/Scripts/TriangleGenerator.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class TriangleGenerator : Shape
