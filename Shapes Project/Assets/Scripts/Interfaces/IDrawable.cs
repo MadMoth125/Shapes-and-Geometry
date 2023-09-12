c1c905cca76c18fa127b1eea7f6f142fc1ce57df
MadMoth125 <madmoth125@gmail.com>
2023-09-12 14:19:32 -0500

    added GameObject creation/deletion for button presses

--- Shapes Project/Assets/Scripts/Interfaces/IDrawable.cs
+++ Shapes Project/Assets/Scripts/Interfaces/IDrawable.cs
@@ -2,6 +2,7 @@
 {
     public interface IDrawable
     {
-        
+        public void DestroySelf() { }
+
     }
 }
--- Shapes Project/Assets/Scripts/Managers/ShapeManager.cs
+++ Shapes Project/Assets/Scripts/Managers/ShapeManager.cs
@@ -1,18 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ShapeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private Transform shapePosition;
+	
+	private GameObject _currentShape;
+	private GameObject _savedObject;
+	[SerializeField] private GameObject[] shapePrefabs;
+	
+	public void OnCircleSelected()
+	{
+		if (shapePrefabs.Length == 0)
+		{
+			Debug.LogError("No shape prefabs assigned to ShapeManager!");
+			return;
+		}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		GameObject circlePrefab = FindPrefab("CustomCircle");
+
+		if (_currentShape == circlePrefab)
+		{
+			return; // No need to instantiate if it's already the selected shape
+		}
+
+		_currentShape = circlePrefab;
+
+		if (_currentShape != null)
+		{
+			Destroy(_savedObject);
+			_savedObject = Instantiate(_currentShape, shapePosition.position, shapePosition.rotation);
+		}
+		else
+		{
+			Debug.LogError("Could not find prefab with name \"CustomCircle\"!");
+		}
+	}
+
+	public void OnRectangleSelected()
+	{
+		if (shapePrefabs.Length == 0)
+		{
+			Debug.LogError("No shape prefabs assigned to ShapeManager!");
+			return;
+		}
+
+		GameObject rectanglePrefab = FindPrefab("CustomRectangle");
+
+		if (_currentShape == rectanglePrefab)
+		{
+			return; // No need to instantiate if it's already the selected shape
+		}
+
+		_currentShape = rectanglePrefab;
+
+		if (_currentShape != null)
+		{
+			Destroy(_savedObject);
+			_savedObject = Instantiate(_currentShape, shapePosition.position, shapePosition.rotation);
+		}
+		else
+		{
+			Debug.LogError("Could not find prefab with name \"CustomRectangle\"!");
+		}
+	}
+	
+	public void OnTriangleSelected()
+	{
+		if (shapePrefabs.Length == 0)
+		{
+			Debug.LogError("No shape prefabs assigned to ShapeManager!");
+			return;
+		}
+
+		GameObject trianglePrefab = FindPrefab("CustomTriangle");
+
+		if (_currentShape == trianglePrefab)
+		{
+			return; // No need to instantiate if it's already the selected shape
+		}
+
+		_currentShape = trianglePrefab;
+
+		if (_currentShape != null)
+		{
+			Destroy(_savedObject);
+			_savedObject = Instantiate(_currentShape, shapePosition.position, shapePosition.rotation);
+		}
+		else
+		{
+			Debug.LogError("Could not find prefab with name \"CustomTriangle\"!");
+		}
+	}
+	
+	private bool FindPrefab(string prefabName, out GameObject prefab)
+	{
+		prefab = shapePrefabs.FirstOrDefault(shapePrefab => shapePrefab.name == prefabName);
+		return prefab != null;
+	}
+	
+	private GameObject FindPrefab(string prefabName)
+	{
+		return shapePrefabs.FirstOrDefault(shapePrefab => shapePrefab.name == prefabName);
+	}
 }
