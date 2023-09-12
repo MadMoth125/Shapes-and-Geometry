2191b4d484b6c80424122397f239e13173b3a4d2
MadMoth125 <madmoth125@gmail.com>
2023-09-12 10:26:55 -0500

    added constructor to triangle class and comments to classes

--- Shapes Project/Assets/Scripts/Shape.cs
+++ Shapes Project/Assets/Scripts/Shape.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
+	/// <summary>
+	/// The area of the shape.
+	/// </summary>
 	public float Area { get; protected set; }
+	
+	/// <summary>
+	/// The perimeter of the shape.
+	/// </summary>
 	public float Perimeter { get; protected set; }
 	
 	public Shape() { }
 	
+	/// <summary>
+	/// Calculates and returns the area of the shape.
+	/// </summary>
+	/// <returns>The area of the shape.</returns>
 	public virtual float GetShapeArea()
 	{
 		return 0;
 	}
 	
+	/// <summary>
+	/// Calculates and returns the perimeter of the shape.
+	/// </summary>
+	/// <returns>The area of the perimeter.</returns>
 	public virtual float GetShapePerimeter()
 	{
 		return 0;
--- Shapes Project/Assets/Scripts/Triangle.cs
+++ Shapes Project/Assets/Scripts/Triangle.cs
@@ -1,21 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+[Tooltip("This class uses MeshGeneration to render a triangle instead of a SpriteRenderer.")]
 public class Triangle : Shape
 {
+	public Triangle(float legWidth, float legHeight, bool autoOffset = true)
+	{
+		this.legWidth = legWidth;
+		this.legHeight = legHeight;
+		this.autoOffset = autoOffset;
+	}
+	
 	[Range(0.1f, 10.0f)]
-	public float legWidth = 1.0f;
+	public float legWidth;
 	[Range(0.1f, 10.0f)]
-	public float legHeight = 1.0f;
-	public float Hypotenuse { get; protected set; }
+	public float legHeight;
+	public bool autoOffset;
 	private Vector3 LegWidthVector => new(legWidth, 0f, 0f);
 	private Vector3 LegHeightVector => new(0f, legHeight, 0f);
-
-	public bool autoOffset = false;
 	
-	private void Start()
+	/// <summary>
+	/// The hypotenuse of the triangle.
+	/// </summary>
+	public float Hypotenuse { get; protected set; }
+	
+	private void Awake()
 	{
 		InitMesh();
 		GenerateTriangle();
@@ -25,7 +38,7 @@ public class Triangle : Shape
 	{
 		GenerateTriangle();
 	}
-
+	
 	public override float GetShapeArea()
 	{
 		Area = (legWidth * legHeight) / 2;
@@ -38,6 +51,10 @@ public class Triangle : Shape
 		return Perimeter;
 	}
 	
+	/// <summary>
+	/// Calculates and returns the hypotenuse of the triangle.
+	/// </summary>
+	/// <returns>The hypotenuse of the triangle</returns>
 	public float GetHypotenuse()
 	{
 		Hypotenuse = Mathf.Sqrt(Mathf.Pow(legWidth, 2) + Mathf.Pow(legHeight, 2));
@@ -88,6 +105,7 @@ public class Triangle : Shape
 	
 	private void UpdateVerts()
 	{
+		// If autoOffset is true, offset the mesh so that the origin is at the center of the triangle
 		if (autoOffset)
 		{
 			_meshOffset.x = legWidth / 2;
@@ -100,14 +118,14 @@ public class Triangle : Shape
 		}
 		
 		_vertices[0] = Vector3.zero; // Vertex at the origin
-		
-		_vertices[1] = LegWidthVector; // Vertex at the end of the legWidth
-		
+		_vertices[1] = -LegWidthVector; // Vertex at the end of the legWidth
 		_vertices[2] = LegHeightVector; // Vertex at the end of the legHeight
-
+		
+		// applying the offset to the vertices in their respective axes
 		for (int i = 0; i < _vertices.Length; i++)
 		{
-			_vertices[i] -= _meshOffset;
+			_vertices[i].x += _meshOffset.x;
+			_vertices[i].y -= _meshOffset.y;
 		}
 	}
 
@@ -115,7 +133,7 @@ public class Triangle : Shape
 	{
 		for (int i = 0; i < 3; i++)
 		{
-			_normals[i] = -Vector3.forward;
+			_normals[i] = Vector3.forward;
 		}
 	}
 	
