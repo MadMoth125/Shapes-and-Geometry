03416dcb474603a0f7a2922ab33c81320c3faa6c
MadMoth125 <madmoth125@gmail.com>
2023-09-12 12:19:18 -0500

    updated using statements for all shape classes

--- Shapes Project/Assets/Scripts/Circle.cs
+++ Shapes Project/Assets/Scripts/Circle.cs
@@ -1,25 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+
+[ExecuteInEditMode]
+
+#endif
 
 public class Circle : Shape
 {
-	public float Radius { get; private set; }
-	
 	public Circle(float radius)
 	{
-		Radius = radius;
+		this.radius = radius;
+	}
+	
+	public float radius;
+	private float _previousRadius;
+	
+	private bool ShouldUpdate => radius != _previousRadius;
+	
+	private void Update()
+	{
+		if (ShouldUpdate)
+		{
+			_previousRadius = radius;
+			
+			transform.localScale = new Vector3(radius * 2f, radius * 2f, 1f);
+		}
 	}
 	
 	public override float GetShapeArea()
 	{
-		Area = Mathf.PI * Mathf.Pow(Radius, 2);
+		Area = Mathf.PI * Mathf.Pow(radius, 2);
 		return Area;
 	}
 	
 	public override float GetShapePerimeter()
 	{
-		Perimeter = 2 * Mathf.PI * Radius;
+		Perimeter = 2 * Mathf.PI * radius;
 		return Perimeter;
 	}
 }
--- Shapes Project/Assets/Scripts/Rectangle.cs
+++ Shapes Project/Assets/Scripts/Rectangle.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 #if UNITY_EDITOR
@@ -12,40 +11,42 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class Rectangle : Shape
 {
-    public float length;
-    public float width;
-    
-    private float _previousLength;
-    private float _previousWidth;
-    
-    private SpriteRenderer _spriteRenderer;
-    [SerializeField] private Sprite sprite;
-    
-    public Rectangle(float length, float width)
+    public Rectangle(float width, float height)
     {
-        this.length = length;
         this.width = width;
+        this.height = height;
     }
+    
+    [Range(0.1f, 10.0f)]
+    public float width;
+    [Range(0.1f, 10.0f)]
+    public float height;
+    
+    private float _previousWidth;
+    private float _previousHeight;
 
+    private bool ShouldUpdate => height != _previousHeight || width != _previousWidth;
+    
     private void Update()
     {
-        if (_previousLength != length || _previousWidth != width)
+        if (ShouldUpdate)
         {
-            transform.localScale = new Vector3(length, width, 1f);
-            _previousLength = length;
             _previousWidth = width;
+            _previousHeight = height;
+            
+            transform.localScale = new Vector3(width, height, 1f);
         }
     }
 
     public override float GetShapeArea()
     {
-        Area = length * width;
+        Area = height * width;
         return Area;
     }
     
     public override float GetShapePerimeter()
     {
-        Perimeter = 2 * (length + width);
+        Perimeter = 2 * (height + width);
         return Perimeter;
     }
 }
