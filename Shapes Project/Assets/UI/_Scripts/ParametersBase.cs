f79f0fac7e4a6635611a6138b1215b28e85b9fba
MadMoth125 <madmoth125@gmail.com>
2023-09-13 23:06:45 -0500

    Update ParametersBase.cs

--- Shapes Project/Assets/UI/_Scripts/ParametersBase.cs
+++ Shapes Project/Assets/UI/_Scripts/ParametersBase.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Shapes;
 using UnityEngine;
 
+// Base class for all parameters scripts.
 public class ParametersBase : MonoBehaviour
 {
 	public Shape shapeRef;
-
-	// Base class for all parameters scripts.
 }
